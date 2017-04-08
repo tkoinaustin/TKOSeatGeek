8ab4fffac269eb19eeb47192628f5c0110a526f3
Tom Nelson <tkoinaustin@yahoo.com>
2017-04-08 06:59:00 -0500

    Search view and  link from Intro view

---
+++ TKOSeatGeek/SearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewModel.swift
+//  TKOSeatGeek
+//
+//  Created by Tom Nelson on 4/8/17.
+//  Copyright © 2017 TKO Solutions. All rights reserved.
+//
+
+import UIKit
+class SearchViewModel {
+  
+}
---
+++ TKOSeatGeek/Views/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  TKOSeatGeek
+//
+//  Created by Tom Nelson on 4/6/17.
+//  Copyright © 2017 TKO Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+  
+  override func viewDidLoad() {
+
+  }
+  
+}
